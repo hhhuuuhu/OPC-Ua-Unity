abe4b82bd28ab839f4d20ba21e4802992f60f95a
sooaaib <soaib.safi@gmail.com>
2020-08-24 20:17:52 +0200

    opc-ua server-client interface app

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public TMP_InputField tmpInputField;
+    public TMP_Dropdown tmpDropDown;
+    Client client = new Client();
+    ServerNode node;
+
+
+    public void DisplayServerData()
+    {
+        int val = tmpDropDown.value;
+        
+        if(val == 0)
+        {
+            if (IsInvoking("Display"))
+            {
+                CancelInvoke("Display");
+            }
+            tmpInputField.text = "Please select a server";
+        }
+        if (val == 1)
+        {
+            InvokeRepeating("Display", 0.5f, 0.5f);
+        }
+        if (val == 2)
+        {
+            if (IsInvoking("Display"))
+            {
+                CancelInvoke("Display");
+            }
+            tmpInputField.text = "This server is not available now!";
+        }
+        if (val == 3)
+        {
+            if (IsInvoking("Display"))
+            {
+                CancelInvoke("Display");
+            }
+            tmpInputField.text = "This server is not available now!";
+        }
+
+    }
+    async void Display()
+    {
+
+        string nodeId = "ns=2;i=2";
+        node = await client.Fetchdata(nodeId);
+        tmpInputField.text = node.Nodename.ToString()+ ": "+node.Nodevalue.ToString();
+    }
+}
---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Opc.Ua.Client;
+using Opc.Ua;
+using Opc.Ua.Configuration;
+using System;
+using System.Threading.Tasks;
+
+public class Client
+{
+    private ApplicationConfiguration config;
+    public double dataFromServer;
+    public Session session;
+
+    public async void Init()
+    {
+        config = new ApplicationConfiguration()
+        {
+            ApplicationName = "Test-Client",
+            ApplicationType = ApplicationType.Client,
+            SecurityConfiguration = new SecurityConfiguration { ApplicationCertificate = new CertificateIdentifier() },
+            TransportConfigurations = new TransportConfigurationCollection(),
+            TransportQuotas = new TransportQuotas { OperationTimeout = 15000 },
+            ClientConfiguration = new ClientConfiguration { DefaultSessionTimeout = 60000 }
+        };
+
+        await config.Validate(ApplicationType.Client);
+        if (config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+        {
+            config.CertificateValidator.CertificateValidation += (s, e) => { e.Accept = (e.Error.StatusCode == StatusCodes.BadCertificateUntrusted); };
+        }
+    }
+    public async Task<ServerNode> Fetchdata(string nodeId)
+    {
+        Init();
+        using (session = await Session.Create(config, new ConfiguredEndpoint(null, new EndpointDescription("opc.tcp://127.0.0.1:4840/")), true, "", 60000, null, null))
+        {
+            var val = session.ReadValue(NodeId.Parse("ns=2;i=2"));
+            double value = Math.Round(Convert.ToDouble(val.ToString()), 3);
+            string name = Convert.ToString(session.ReadNode(nodeId));
+            ServerNode serverNode = new ServerNode(name, value);
+            return serverNode;
+            //return referenceDescriptions;
+        }
+    }
+}
+
+public class ServerNode
+{
+    private string nodename;
+    private double nodeValue;
+
+    public ServerNode(string name, double value)
+    {
+        this.nodename = name;
+        this.nodeValue = value;
+    }
+    public string Nodename
+    {
+        get { return nodename; }
+    }
+
+    public double Nodevalue
+    {
+        get { return nodeValue; }
+    }
+}
